6bbbbf4f37961d06e8808540715fb82f8de9cdf5
fengyaoshun <fengyaoshun@redorcasz.com>
2024-10-25 16:42:19 +0800

    1111

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import *
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("index/", IndexView.as_view(), name="index"),
     path("about/", AboutPageView.as_view(), name="about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,11 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "pages/home.html"
+    template_name = "base/system_base.html"
+
+
+class IndexView(TemplateView):
+    template_name = "RASystem/index.html"
 
 
 class AboutPageView(TemplateView):
